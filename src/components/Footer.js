51791498ddbec5b59e385fb3081b01fd9719f149
Andrew Alexander <account+github@adrw.ch>
2018-12-08 16:09:08 -0500

    README

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = class extends React.Component {
       <footer class="footer">
         <div class="content has-text-centered">
           &copy; 2009-{dayjs().year()}{" "}
-          <a href="https://adrw.xyz/">ADRW Digital</a> |
+          <a href="https://adrw.xyz/">ADRW Digital</a> |{" "}
           <a href="https://andrew.fm">Andrew Paradi Alexander</a>
         </div>
       </footer>
